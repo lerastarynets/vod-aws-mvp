7706e9f7c2d8a3f5ee60ed766c73f1f152ef7573
valeriiaKoitechs <valeria.koitechs@gmail.com>
2026-01-01 15:06:20 +0200

    refactor: remove console log

--- apps/lambdas/mediaconvert-notify/handler.ts
+++ apps/lambdas/mediaconvert-notify/handler.ts
@@ -20,7 +20,6 @@ export const handler = async (event: EventBridgeEvent) => {
     const videoId = detail.userMetadata?.videoId;
 
     console.log(`Received event for video ${videoId} with status ${status}`);
-    console.log(`User metadata: ${detail.userMetadata}`);
     if (!videoId) return;
 
     if (status === "COMPLETE") {
